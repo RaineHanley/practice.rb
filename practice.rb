2da87b55fd0d06a35e4d4195ba7fb7a9a499c914
RaineHanley <rainehanley@gmail.com>
2023-05-09 13:37:41 -0700

    updates so players can collect stars

--- practice.rb
+++ practice.rb
@@ -1,4 +1,4 @@
-require "gosu"
+require "Gosu"
 
 class Tutorial < Gosu::Window
   def initialize
@@ -6,13 +6,102 @@ class Tutorial < Gosu::Window
     self.caption = "Tutorial Game"
 
     @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+    @player = Player.new
+    @player.warp(320, 240)
   end
 
   def update
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @player.accelerate
+    end
+    @player.move
   end
 
   def draw
+    @player.draw
     @background_image.draw(0, 0, 0)
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_ESCAPE
+      close
+    else
+      super
+    end
+  end
+end
+
+class Star
+  attr_reader :x, :y
+
+  def initialize(animation)
+    @animation = animation
+    @color = Gosu::Color::BLACK.dup
+    @color.red = rand(256 - 40) + 40
+    @color.green = rand(256 - 40) + 40
+    @color.blue = rand(256 - 40) + 40
+    @x = rand * 640
+    @y = rand * 480
+  end
+
+  def draw
+    img = @animation[Gosu.milliseconds / 100 % @animation.size]
+    img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
+             ZOrder::STARS, 1, 1, @color, :add)
+  end
+end
+
+class Player
+  def initialize
+    @image = Gosu::Image.new("media/starfighter.bmp")
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @score = 0
+  end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+
+  def turn_left
+    @angle -= 4.5
+  end
+
+  def turn_right
+    @angle += 4.5
+  end
+
+  def accelerate
+    @vel_x += Gosu.offset_x(@angle, 0.5)
+    @vel_y += Gosu.offset_y(@angle, 0.5)
+  end
+
+  def move
+    @x += @vel_x
+    @y += @vel_y
+    @x %= 640
+    @y %= 480
+
+    @vel_x *= 0.95
+    @vel_y *= 0.95
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 1, @angle)
+  end
+
+  def score
+    @score
+  end
+
+  def collect_stars(stars)
+    stars.reject! { |star| Gosu.distance(@x, @y, star.x, star.y) < 35 }
   end
 end
 
