e26d52aadb331c18d8badb7ed396a8e67dec272d
RaineHanley <rainehanley@gmail.com>
2023-05-10 14:04:18 -0700

    figuring out extending window to load animation

--- practice.rb
+++ practice.rb
@@ -13,93 +13,53 @@ class Tutorial < Gosu::Window
 
     @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
     @stars = Array.new
-
-    @font = Gosu::Font.new(20)
   end
-
-  def draw
-    @background_image.draw(0, 0, ZOrder::BACKGROUND)
-    @player.draw
-    @stars.each { |star| star.draw }
-    @font.draw_text("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-  end
-
   def update
-    @player.move
-    @player.collect_stars(@stars)
-    if rand(100) < 4 && @stars.size < 25
-      @stars.push(Star.new(@star_anim))
+
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @player.turn_left
     end
-  end
-
-  def button_down(id)
-    if id == Gosu::KB_ESCAPE
-      close
-    else
-      super
+      if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+        @player.turn_right
+      end
+      if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+        @player.accelerate
+      end
+      @player.move
+      @player.move
+      @player.collect_stars(@stars)
+  
+      if rand(100) < 4 and @stars.size < 25
+        @stars.push(Star.new(@star_anim))
+      end
     end
-  end
-end
-
-Tutorial.new.show
-
-# creating players
-class Player
-  attr_reader :score
-
-  def initialize
-    @image = Gosu::Image.new("media/starfighter.bmp")
-    @x = @y = @vel_x = @vel_y = @angle = 0.0
-    @score = 0
-  end
-
-  def score
-    @score
-  end
-
-  def warp(x, y)
-    @x, @y = x, y
-  end
-
-  def turn_left
-    @angle -= 4.5
-  end
-
-  def turn_right
-    @angle += 4.5
-  end
-
-  def accelerate
-    @vel_x += Gosu.offset_x(@angle, 0.5)
-    @vel_y += Gosu.offset_y(@angle, 0.5)
-  end
-
-  def move
-    @x += @vel_x
-    @y += @vel_y
-    @x %= 640
-    @y %= 480
-
-    @vel_x *= 0.95
-    @vel_y *= 0.95
-  end
-
-  def draw
-    @image.draw_rot(@x, @y, 1, @angle)
-  end
-
-  def collect_stars(stars)
-    stars.reject! do |star|
-      if Gosu.distance(@x, @y, star.x, star.y) < 35
-        @score += 10
-        @beep.play
-        true
+  
+    def draw
+      @background_image.draw(0, 0, ZOrder::BACKGROUND)
+      @player.draw
+      @stars.each { |star| star.draw }
+    end
+    end
+  
+    def draw
+      @player.draw
+      @background_image.draw(0, 0, 0)
+    end
+  
+    def button_down(id)
+      if id == Gosu::KB_ESCAPE
+        close
       else
-        false
+        super
       end
     end
   end
+end
+  Tutorial.new.show
 
+  module ZOrder
+    BACKGROUND, STARS, PLAYER, UI = *0..3
+  end
   # creating stars
   class Star
     attr_reader :x, :y
@@ -119,5 +79,51 @@ class Player
       img.draw(@x - img.width / 2.0, @y - img.height / 2.0,
                ZOrder::STARS, 1, 1, @color, :add)
     end
+
+# creates player class
+class Player
+  def initialize
+    @image = Gosu::Image.new("media/starfighter.bmp")
+    @x = @y = @vel_x = @vel_y = @angle = 0.0
+    @score = 0
   end
+
+  def warp(x, y)
+    @x, @y = x, y
+  end
+  
+  def turn_left
+    @angle -= 4.5
+  end
+  
+  def turn_right
+    @angle += 4.5
+  end
+  
+  def accelerate
+    @vel_x += Gosu.offset_x(@angle, 0.5)
+    @vel_y += Gosu.offset_y(@angle, 0.5)
+  end
+  
+  def move
+    @x += @vel_x
+    @y += @vel_y
+    @x %= 640
+    @y %= 480
+    
+    @vel_x *= 0.95
+    @vel_y *= 0.95
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 1, @angle)
+  end
+  def score
+    @score
+  end
+
+  def collect_stars(stars)
+    stars.reject! { |star| Gosu.distance(@x, @y, star.x, star.y) < 35 }
+  end
+  
 end
